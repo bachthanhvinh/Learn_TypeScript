13f9713c8192a7b00aa0f09aa313cf6cb7d446dd
Vinhz <bachthanhvinh12@gmail.com>
2025-07-19 00:00:56 +0700

    Learn Generic

--- lesson1/build/js/Lesson7/lesson7.js
+++ lesson1/build/js/Lesson7/lesson7.js
@@ -1,42 +1,70 @@
 "use strict";
-// Index Signatures tạo ra một key lấy giá trị động 
-const todaysTransactions = {
-    Pizza: -10,
-    Books: -5,
-    Job: 50
-};
-// console.log(todaysTransactions.Pizza)
-// console.log(todaysTransactions['Pizza'])
-let prop = 'Pizza';
-// console.log(todaysTransactions[prop])
-const todaysNet = (transactions) => {
-    let total = 0;
-    for (const transaction in transactions) {
-        total += transactions[transaction];
-    }
-    return total;
-};
-const student = {
-    name: 'vinh',
-    GPA: 3.7,
-    class: ['dev', 'developer']
-};
-// console.log(students.test)
-/////////// keyof Assertions lấy giá trị key theo kiểu union "vinh"| "lop" | "typeScript"
-for (const key in student) {
-    // console.log(`${key}: ${student[key as keyof Student]} `) 
-}
-Object.keys(student).map(key => {
-    // console.log(`${student[key as keyof Student]}`)
-});
-const ElementStudent = (student, key) => {
-    //  console.log(`Item ${key}: ${student[key]}`)
-};
-const monthlyIncomes = {
-    salary: 1000,
-    bonus: 50,
-    sidehustle: 250
-};
-for (const revenue in monthlyIncomes) {
-    console.log(monthlyIncomes[revenue]);
-}
+// // Index Signatures tạo ra một key lấy giá trị động 
+// // interface TransactionObj {
+// //    readonly [index: string]: number
+// // }
+// interface TransactionObj {
+//   readonly [index: string]: number
+//   Pizza: number,
+//   Books: number,
+//   Job: number
+// }
+// const todaysTransactions: TransactionObj = {
+//   Pizza: -10,
+//   Books: -5,
+//   Job: 50
+// }
+// // console.log(todaysTransactions.Pizza)
+// // console.log(todaysTransactions['Pizza'])
+// let prop: string = 'Pizza'
+// // console.log(todaysTransactions[prop])
+// const todaysNet = (transactions: TransactionObj):
+// number => {
+//   let total = 0
+//   for (const transaction in transactions) {
+//       total += transactions[transaction]
+//   }
+//   return total
+// }
+// // console.log(todaysNet(todaysTransactions))
+// // todaysTransactions.Pizza = 40
+// // console.log(todaysTransactions["vinh"])
+// ////////////////////// 
+// interface Student {
+//     // readonly [key: string]: string | number | string[]
+//     // | undefined
+//     name: string,
+//     GPA: number,
+//     class?: string[]
+// }
+// const student : Student = {
+//     name: 'vinh',
+//     GPA: 3.7,
+//     class: ['dev','developer']
+// }
+// // console.log(students.test)
+// /////////// keyof Assertions lấy giá trị key theo kiểu union "vinh"| "lop" | "typeScript"
+// for (const key in student) {
+//     // console.log(`${key}: ${student[key as keyof Student]} `) 
+// }
+// Object.keys(student).map(key => {
+//     // console.log(`${student[key as keyof Student]}`)
+// }
+// )
+// const ElementStudent = (student: Student , key: keyof Student): void => {
+//     //  console.log(`Item ${key}: ${student[key]}`)
+// }
+// // ElementStudent(student, 'name')
+// // interface Incomes {
+// //      [key: string]: number
+// // }
+// type Streams = 'salary' | 'bonus' | 'sidehustle'
+// type Incomes = Record<Streams, number | string>
+// const monthlyIncomes: Incomes = {
+//      salary: 1000,
+//      bonus: 50,
+//      sidehustle: 250
+// }
+// for(const revenue in monthlyIncomes) {
+//      console.log(monthlyIncomes[revenue as keyof Incomes])
+// }
---
+++ lesson1/build/js/Lesson8/lesson8.js
@@ -0,0 +1,105 @@
+"use strict";
+// const  echo =  <T>(arg: T): T => arg
+// const isObj = <T>(arg: T): boolean => {
+//      return (typeof arg === 'object' && !Array.isArray(arg) 
+//      && arg !== null
+//      )
+// }
+// console.log(isObj(true))
+// console.log(isObj('vinh'))
+// console.log(isObj([1,2,3]))
+// console.log(isObj({name: 'John'}))
+// console.log(isObj(null))
+// // const isTrue = <T>(arg: T): {arg: T, is: boolean} => {
+// //     if (Array.isArray(arg) && !arg.length) {
+// //         return { arg, is: false}
+// //     }
+// //     if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+// //         return { arg, is: false}
+// //     }
+// //     return { arg, is: !!arg}
+// // }
+// // console.log(isTrue(false));
+// // console.log(isTrue(0));
+// // console.log(isTrue(true));
+// // console.log(isTrue(1));
+// // console.log(isTrue('vinh'));
+// // console.log(isTrue(''));
+// // console.log(isTrue(null));
+// // console.log(isTrue(undefined));
+// // console.log(isTrue({}));
+// // console.log(isTrue({ name: "thanhvinh"}));
+// // console.log(isTrue([1, 2, 3]));
+// // console.log(isTrue(NaN));
+// // console.log(isTrue(-0));
+// interface BoolCheck<T> {
+//       value: T,
+//       is: boolean 
+// }
+// const CheckBoolValue = <T>(arg: T): BoolCheck<T> => {
+//     if (Array.isArray(arg) && !arg.length) {
+//         return { value: arg, is: false}
+//     }
+//     if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+//         return { value: arg, is: false}
+//     }
+//     return { value: arg, is: !!arg}
+// }
+// console.log(CheckBoolValue(false));
+// console.log(CheckBoolValue(0));
+// console.log(CheckBoolValue(true));
+// console.log(CheckBoolValue(1));
+// console.log(CheckBoolValue('vinh'));
+// console.log(CheckBoolValue(''));
+// console.log(CheckBoolValue(null));
+// console.log(CheckBoolValue(undefined));
+// console.log(CheckBoolValue({}));
+// console.log(CheckBoolValue({ name: "thanhvinh"}));
+// console.log(CheckBoolValue([1, 2, 3]));
+// console.log(CheckBoolValue(NaN));
+// console.log(CheckBoolValue(-0));
+// const processUser = <T extends HasID>(user: T): T => 
+// {
+//     // process the user with logic here
+//     return user
+// }
+// console.log(processUser({ id: 1, name: 'vinh'}))
+// interface HasID {
+//     id: number
+// }
+// const getUserPropert = <T extends HasID, K extends keyof T>(user: T[], key: K): T[K][] => {
+//     return user.map(item => item[key])
+// }
+// const users = [
+//     {
+//         id: 1,
+//         name: 'vinh',
+//         email: "bachthanhvinh12@gmail.com"
+//     },
+//     {
+//         id: 2,
+//         name: 'bachthanhvinh',
+//         email: "Thanhvinh@gmail.com"
+//     }
+// ]
+// console.log(getUserPropert(users, 'name'))
+class Product {
+    constructor(value) {
+        this.data = value;
+    }
+    get state() {
+        return this.data;
+    }
+    set state(value) {
+        this.data = value;
+    }
+}
+const myProduct = new Product("vinh");
+console.log(myProduct.state);
+myProduct.state = 'thanhvinh';
+// myProduct.state = 21
+console.log(myProduct.state);
+const myNumber = new Product([15]);
+console.log(myNumber.state);
+myNumber.state = ['thanhvinh', 18, true];
+console.log(myNumber.state);
